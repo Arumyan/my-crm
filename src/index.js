7f9cdf6452164a79c039233f44c32fb4158cba1e
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-09-02 08:37:24 +0300

    add auth

--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 //import 'materialize-css/dist/js/materialize';
-
 import App from './App';
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('app')
-);
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyB2la3L0Ec6Eq06WYoA6_S1BYsFBihyiM0",
+  authDomain: "my-crm-b693d.firebaseapp.com",
+  databaseURL: "https://my-crm-b693d.firebaseio.com",
+  projectId: "my-crm-b693d",
+  storageBucket: "my-crm-b693d.appspot.com",
+  messagingSenderId: "342950837669",
+  appId: "1:342950837669:web:bbb2a0c95beb8dde653c4c"
+});
+
+
+firebase.auth().onAuthStateChanged(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('app')
+  );
+
+  
+})
+
+
+
 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink, Redirect } from 'react-router-dom';
+
+import firebase from 'firebase/app';
 
 const Login = () => {
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = async (email, password) => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      setIsLogin(true);
+    } catch (e) {
+      //console.log(e);
+    }
+  };
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    login(email, password)
+  };
+
+  if(isLogin) {
+    return <Redirect to='/' />
+  }
+
   return (
-    <form className='card auth-card'>
+    <form className='card auth-card' onSubmit={handleSumbit}>
       <div className='card-content'>
         <span className='card-title'>Домашняя бухгалтерия</span>
+
         <div className='input-field'>
-          <input id='email' type='text' className='validate' />
-          <label for='email'>Email</label>
+          <input
+            id='email'
+            type='text'
+            value={email}
+            className='validate'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor='email'>Email</label>
           <small className='helper-text invalid'>Email</small>
         </div>
+
         <div className='input-field'>
-          <input id='password' type='password' className='validate' />
-          <label for='password'>Пароль</label>
+          <input
+            id='password'
+            type='password'
+            value={password}
+            className='validate'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor='password'>Пароль</label>
           <small className='helper-text invalid'>Password</small>
         </div>
       </div>
