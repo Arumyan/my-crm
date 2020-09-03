f683cb381cdb1eec8f93ed268e5a8a03db7c06f9
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-09-03 07:51:51 +0300

    some changes

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,10 +1,14 @@
+// ACTION
+//----------------------------------------------//
+export const SET_AUTH_DATA = 'auth/SET_AUTH_DATA';
+
+// REDUCER
+//----------------------------------------------//
 const initialState = {
   email: '',
   password: '',
   isAuth: false,
 };
-
-export const SET_AUTH_DATA = 'auth/SET_AUTH_DATA';
 
 export default function authReducer(state = initialState, action) {
   switch (action.type) {
@@ -20,7 +24,8 @@ export default function authReducer(state = initialState, action) {
   }
 }
 
-// ACTIONS
+// ACTION CREATOR
+//----------------------------------------------//
 export const setAuthDataAction = ({email, password, isAuth}) => {
   return { type: SET_AUTH_DATA, payload: {email, password, isAuth}}
 }
