7a529fdde6c1b0b4747241449cac84b5bd989068
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-09-05 13:37:36 +0300

    wip

--- src/layout/MainLayout/MainLayout.js
+++ src/layout/MainLayout/MainLayout.js
@@ -3,6 +3,7 @@ import './MainLayout.scss'
 import Nav from '../../components/Nav/Nav'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import BtnAddNewItem from '../../components/BtnAddNewItem/BtnAddNewItem'
+import { Redirect } from 'react-router-dom';
 
 import { useSelector, useDispatch } from 'react-redux';
 import {infoAPI} from '../../api/infoAPI'
@@ -10,22 +11,30 @@ import { setInfoAction } from '../../redux/reducers/infoReducer';
 
 const MainLayout = (props) => {
 
+  const isAuth = useSelector((state) => state.authReducer.isAuth);
   const dispatch = useDispatch();
 
   useEffect(() => {
     infoAPI.fetchInfo().then((data) => {
       dispatch(setInfoAction(data))
+    }).catch((e) => {
+      console.log(e)
     })
   }, [dispatch])
 
   const [isOpen, setIsOpen] =  useState(true);
   const info = useSelector((state) => state.infoReducer.info);
+ 
 
   const toggleNav = () => {
     setIsOpen(!isOpen);
   }
 
   let appContentClasses = isOpen ? '' : ' full';
+
+  if (!isAuth) {
+    return <Redirect to='/login' />;
+  }
 
   return (
     <div className="app-main-layout">
