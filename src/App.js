be5fb8a796ca7667f774c59480d23cb6df49211e
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-09-06 10:30:08 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
 
 import { routes } from './routes/routes';
 import AppRoute from './hoc/AppRoute';
-
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
+
+import {infoAPI} from './api/infoAPI'
+import { setInfoAction } from './redux/reducers/infoReducer';
+import firebase from 'firebase/app';
+import { useDispatch } from 'react-redux';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log('init app')
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        infoAPI.fetchInfo().then((data) => {
+          dispatch(setInfoAction(data))
+        }).catch((e) => {
+          console.log(e)
+        })
+      } else {
+        console.log('Пользователь не авторизован')
+      }
+    });
+  })
+
   return (
     <Router>
       <Switch>
--- src/layout/MainLayout/MainLayout.js
+++ src/layout/MainLayout/MainLayout.js
@@ -1,34 +1,14 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import './MainLayout.scss'
 import Nav from '../../components/Nav/Nav'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import BtnAddNewItem from '../../components/BtnAddNewItem/BtnAddNewItem'
 //import { Redirect } from 'react-router-dom';
 
-import { useSelector, useDispatch } from 'react-redux';
-import {infoAPI} from '../../api/infoAPI'
-import { setInfoAction } from '../../redux/reducers/infoReducer';
+import { useSelector} from 'react-redux';
 
-import firebase from 'firebase/app';
 
 const MainLayout = (props) => {
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        infoAPI.fetchInfo().then((data) => {
-          dispatch(setInfoAction(data))
-        }).catch((e) => {
-          console.log(e)
-        })
-      } else {
-        console.log('Пользователь не авторизован')
-      }
-    });
-  }, [dispatch])
 
   const [isOpen, setIsOpen] =  useState(true);
   const info = useSelector((state) => state.infoReducer.info);
