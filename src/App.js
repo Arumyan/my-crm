9459c8a2fded028fd8797bd02eaf2c064670eca7
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-09-18 18:50:51 +0300

    add protected routes

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ const App = () => {
               path={route.path}
               exact={route.exact}
               layout={route.layout}
+              authRequire={route.authRequire}
               component={route.component}
             />
           );
